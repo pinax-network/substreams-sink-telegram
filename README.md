6394198b8f929f50efb9881ea9470aabd0123820
Charles Morin <charles.mor@outlook.fr>
2023-03-21 11:34:46 -0400

    First commit :tada:

---
+++ README.md
@@ -0,0 +1,41 @@
+# [`Substreams`](https://substreams.streamingfast.io/) Socials CLI `Node.js`
+
+<!-- [<img alt="github" src="" height="20">](https://github.com/pinax-network/substreams-sink-socials) -->
+<!-- [<img alt="npm" src="" height="20">](https://www.npmjs.com/package/substreams-sink-socials) -->
+<!-- [<img alt="GitHub Workflow Status" src="" height="20">](https://github.com/pinax-network/substreams-sink-socials/actions?query=branch%3Amain) -->
+
+> `substreams-sink-socials` is a tool that allows developers to pipe data extracted from a blockchain to their favorite messaging social platform.
+
+## 📖 Documentation
+
+<!-- ### https://www.npmjs.com/package/substreams-sink-socials -->
+
+### Further resources
+
+- [**Substreams** documentation](https://substreams.streamingfast.io)
+
+### Protobuf
+
+## CLI
+[**Use pre-built binaries**](https://github.com/pinax-network/substreams-sink-socials/releases)
+- [x] MacOS
+- [x] Linux
+- [x] Windows
+
+**Install** globally via npm
+```
+$ npm install -g substreams-sink-socials
+```
+
+**Run**
+```
+$ substreams-sink-socials run [options] <spkg>
+```
+
+## Features
+
+- Consume `*.spkg` from:
+  - [x] Load URL or IPFS
+  - [ ] Read from `*.spkg` local filesystem
+  - [ ] Read from `substreams.yaml` local filesystem
+- [ ] Handle `cursor` restart
---
+++ index.ts
@@ -0,0 +1,60 @@
+import { Substreams, download, unpack } from "substreams";
+import { timeout } from "./src/utils";
+
+// default substreams options
+export const MESSAGE_TYPE_NAME = 'pinax.substreams.sink.social.v1.Messages'
+export const DEFAULT_API_TOKEN_ENV = 'SUBSTREAMS_API_TOKEN'
+export const DEFAULT_OUTPUT_MODULE = 'social_out'
+export const DEFAULT_SUBSTREAMS_ENDPOINT = 'https://mainnet.eth.streamingfast.io:443'
+
+export async function run(spkg: string, options: {
+    // substreams options
+    outputModule?: string,
+    startBlock?: string,
+    stopBlock?: string,
+    substreamsEndpoint?: string,
+    substreamsApiTokenEnvvar?: string,
+    substreamsApiToken?: string,
+    delayBeforeStart?: string,
+    // user options
+} = {}) {
+    // substreams options
+    const outputModule = options.outputModule ?? DEFAULT_OUTPUT_MODULE
+    const substreamsEndpoint = options.substreamsEndpoint ?? DEFAULT_SUBSTREAMS_ENDPOINT
+    const api_token_envvar = options.substreamsApiTokenEnvvar ?? DEFAULT_API_TOKEN_ENV
+    const api_token = options.substreamsApiToken ?? process.env[api_token_envvar]
+
+    // 
+    if (!outputModule) throw new Error('[output-module] is required')
+    if (!api_token) throw new Error('[substreams-api-token] is required')
+
+    // delay before start
+    if (options.delayBeforeStart) await timeout(Number(options.delayBeforeStart) * 1000);
+
+    // Download Substream from URL or IPFS
+    const binary = await download(spkg);
+
+    // Initialize Substreams
+    const substreams = new Substreams(binary, outputModule, {
+        host: substreamsEndpoint,
+        startBlockNum: options.startBlock,
+        stopBlockNum: options.stopBlock,
+        authorization: api_token,
+    });
+
+    // Find Protobuf message types from registry
+    const { registry } = unpack(binary);
+    const SocialMessages = registry.findMessage(MESSAGE_TYPE_NAME);
+    if (!SocialMessages) throw new Error(`Could not find [${MESSAGE_TYPE_NAME}] message type`);
+
+    substreams.on("mapOutput", (output: any) => {
+        if (!output.data.value.typeUrl.match(MESSAGE_TYPE_NAME)) return;
+        const decoded = SocialMessages.fromBinary(output.data.value.value);
+        for (const socialMessages of decoded.messages) {
+            console.log(socialMessages);
+        }
+    });
+
+    // start streaming Substream
+    await substreams.start();
+}
