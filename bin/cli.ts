726488d417fa87b5bc68557daf7533a49bc2c200
Charles Morin <charles.mor@outlook.fr>
2023-04-24 10:55:34 -0400

    Added config file option

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,15 +1,25 @@
 #!/usr/bin/env node
 
-import { cli } from "substreams-sink";
+import { cli, logger } from "substreams-sink";
+import fs from "fs";
+
 import { action, DEFAULT_TELEGRAM_API_TOKEN_ENV } from "../index.js"
 import pkg from "../package.json";
 
 const program = cli.program(pkg);
 const command = cli.run(program, pkg);
 
-command.requiredOption('-c --chat-id <string>', 'Telegram chat ID')
+command.requiredOption('-c --config <string>', 'Config file path')
 command.option('--telegram-api-token <string>', 'API token for the Telegram bot')
 command.option('--telegram-api-token-envvar <string>', 'Environnement variable name of the API token for the Telegram bot', DEFAULT_TELEGRAM_API_TOKEN_ENV)
 
 command.action(action);
 program.parse();
+
+const options = command.opts();
+if (options.config) {
+    if (!fs.existsSync(options.config)) {
+        logger.error(`file '${options.config}' does not exist`);
+        process.exit(1);
+    }
+}
